58b0f63639a84c55d3427070a9647f6a13dc0a7f
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2025-02-11 21:00:36 +0100

    Add database domain entity tests

---
+++ tests/CleanArchitecture.Domain.Tests/Databases/Entities/DatabaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace CleanArchitecture.Domain.Tests.Databases.Entities;
+
+public class DatabaseTests
+{
+
+    [Fact(DisplayName = "Create a new provisioned database")]
+    public void CreateNewProvisionedDatabase()
+    {
+        var databaseName = "CompanyDb";
+        var database = Database.Create(databaseName);
+
+        database.Should().NotBeNull();
+        database.Name.Should().Be(databaseName);
+        database.IsActive.Should().BeTrue();
+
+    }
+    
+}
