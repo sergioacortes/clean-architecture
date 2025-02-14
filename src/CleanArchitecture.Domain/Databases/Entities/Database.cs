7a1b038b75db151df16c415b6f8a6578f458b8f2
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2025-02-14 13:05:37 +0100

    Update database domain entity with database properties

--- src/CleanArchitecture.Domain/Databases/Entities/Database.cs
+++ src/CleanArchitecture.Domain/Databases/Entities/Database.cs
@@ -4,20 +4,29 @@ namespace CleanArchitecture.Orders.Domain.Databases.Entities;
 
 public class Database : DomainEntity<Guid>
 {
-    private Database(Guid id, string databaseName) : base(id)
+    private Database(Guid id, string serverName, string databaseName, string userName, string password) : base(id)
     {
-        DatabaseName = databaseName;
+        ServerName = serverName ?? throw new ArgumentNullException(nameof(serverName));
+        DatabaseName = databaseName ?? throw new ArgumentNullException(nameof(databaseName));
+        UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        Password = password ?? throw new ArgumentNullException(nameof(password));
         IsActive = true;
     }
+    
+    public string ServerName { get; private set; }
 
     public string DatabaseName { get; private set; }
     
+    public string UserName { get; private set; }
+
+    public string Password { get; private set; }
+
     public bool IsActive { get; private set; }
     
-    public static Database Create(string databaseName)
+    public static Database Create(string serverName, string databaseName, string userName, string password)
     {
         
-        var database = new Database(Guid.NewGuid(), databaseName);
+        var database = new Database(Guid.NewGuid(), serverName, databaseName, userName, password);
         return database;
         
     }
