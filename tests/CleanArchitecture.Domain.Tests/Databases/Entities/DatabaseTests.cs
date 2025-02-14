821fd2f49e49814e95d603172093b53f3e3f4d33
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2025-02-14 13:05:06 +0100

    Add database domain entity tests

--- tests/CleanArchitecture.Domain.Tests/Databases/Entities/DatabaseTests.cs
+++ tests/CleanArchitecture.Domain.Tests/Databases/Entities/DatabaseTests.cs
@@ -10,13 +10,26 @@ public class DatabaseTests
     [Fact(DisplayName = "Create a new provisioned database")]
     public void CreateNewProvisionedDatabase()
     {
-        var databaseName = "CompanyDb";
-        var database = Database.Create(databaseName);
+        var serverName = "(local)";
+        var databaseName = "database";
+        var userName = "admin";
+        var password = "password";
+        var database = Database.Create(serverName, databaseName, userName, password);
 
         database.Should().NotBeNull();
         database.DatabaseName.Should().Be(databaseName);
         database.IsActive.Should().BeTrue();
 
+    }
+    
+    [Theory(DisplayName = "Create a new provisioned database fail")]
+    [InlineData(null, "database", "admin", "password")]
+    [InlineData("(local)", null, "admin", "password")]
+    [InlineData("(local)", "database", null, "password")]
+    [InlineData("(local)", "database", "admin", null)]
+    public void CreateNewProvisionedDatabaseFail(string serverName, string databaseName, string userName, string password)
+    {
+        Assert.Throws<ArgumentNullException>(() => Database.Create(serverName, databaseName, userName, password));
     }
     
 }
