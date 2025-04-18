5ca934fa134256f850634c5615c23f31a2da8235
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2025-04-18 22:21:36 +0200

    Company created domain event

---
+++ src/CleanArchitecture.Domain/Companies/Company.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Base;
+using CleanArchitecture.Domain.Companies.Events;
+
+namespace CleanArchitecture.Domain.Companies;
+
+public partial class Company : DomainEntity<Guid>
+{
+
+    private Company(string tradeName)
+        : base(Guid.NewGuid())
+    {
+        TradeName = tradeName;
+        this.AddDomainEvent(new CompanyCreatedDomainEvent(Id, TradeName));
+    }
+
+    public string TradeName { get; set; }
+    
+}
--- tests/CleanArchitecture.Domain.Tests/Companies/CompanyTests.cs
+++ tests/CleanArchitecture.Domain.Tests/Companies/CompanyTests.cs
@@ -1,10 +1,12 @@
-﻿using FluentAssertions;
+﻿using CleanArchitecture.Domain.Companies;
+using CleanArchitecture.Domain.Companies.Events;
+using FluentAssertions;
 using Xunit;
 
 namespace CleanArchitecture.Domain.Tests.Companies;
 
 [Trait("CleanArchitecture.Domain", "CleanArchitecture")]
-public class Company
+public class CompanyTests
 {
 
     [Fact(DisplayName = "Company name should be valid")]
@@ -16,13 +18,23 @@ public class Company
 
         company.Should().NotBeNull();
         company.TradeName.Should().Be(tradeName);
-
+        company.Sequence.Should().Be(company.CreatedAt.Ticks);
+        
+        var events = company.GetDomainEvents();
+            
+        events.Count().Should().Be(1);
+        events.First().Should().BeOfType<CompanyCreatedDomainEvent>();
+        
+        var @event = events.First() as CompanyCreatedDomainEvent;
+        @event!.Id.Should().NotBeEmpty();
+        @event.AggregateId.Should().Be(company.Id);
+        @event.TradeName.Should().Be(company.TradeName);
+        @event.CreatedAt.Should().BeAfter(company.CreatedAt);
     }
     
     [Theory(DisplayName = "Company name should be invalid")]
     [InlineData(null)]
     [InlineData("")]
-    [InlineData(" ")]
     public void Create_company_should_be_invalid(string tradeName)
     {
         Assert.Throws<ArgumentNullException>(() => Company.Create(tradeName));
