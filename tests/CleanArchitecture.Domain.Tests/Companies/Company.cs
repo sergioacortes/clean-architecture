f9e7b5d0e6ef52ec288c65fbadfde02cec89289c
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2025-04-18 21:31:19 +0200

    Create company unit tests

---
+++ tests/CleanArchitecture.Domain.Tests/Companies/Company.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CleanArchitecture.Domain.Tests.Companies;
+
+[Trait("CleanArchitecture.Domain", "CleanArchitecture")]
+public class Company
+{
+
+    [Fact(DisplayName = "Company name should be valid")]
+    public void Create_company_should_be_valid()
+    {
+
+        var tradeName = "Company trade name";
+        var company = Company.Create(tradeName);
+
+        company.Should().NotBeNull();
+        company.TradeName.Should().Be(tradeName);
+
+    }
+    
+    [Theory(DisplayName = "Company name should be invalid")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Create_company_should_be_invalid(string tradeName)
+    {
+        Assert.Throws<ArgumentNullException>(() => Company.Create(tradeName));
+    }
+
+}
