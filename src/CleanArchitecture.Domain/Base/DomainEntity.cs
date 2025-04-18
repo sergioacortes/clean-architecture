fd24d33c1c8b966794ac1796cac62bd5c109be89
Sergio Antonio Cortes Ocampo <Sergio.cortes@wolterskluwer.com>
2025-04-18 22:21:36 +0200

    Company created implementation

---
+++ src/CleanArchitecture.Domain/Base/DomainEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace CleanArchitecture.Domain.Base;
+
+public abstract class DomainEntity<TType> 
+{
+
+    protected DomainEntity(TType id)
+    {
+        Id = id;
+        CreatedAt = DateTime.UtcNow;
+        Sequence = CreatedAt.Ticks;
+    }
+
+    private List<DomainEvent> DomainEvents { get; } = [];
+    
+    public TType Id { get; private set; }
+    
+    public DateTime CreatedAt { get; private set; }
+    
+    public long Sequence { get; private set; }
+
+    internal void AddDomainEvent(DomainEvent domainEvent) => 
+        DomainEvents.Add(domainEvent);
+    
+    public List<DomainEvent> GetDomainEvents() => 
+        DomainEvents;
+    
+}
---
+++ src/CleanArchitecture.Domain/Base/DomainEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Domain.Base;
+
+public abstract class DomainEvent(Guid id, Guid aggregateId)
+{
+    public Guid Id { get; } = id;
+    public Guid AggregateId { get; } = aggregateId;
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+}
