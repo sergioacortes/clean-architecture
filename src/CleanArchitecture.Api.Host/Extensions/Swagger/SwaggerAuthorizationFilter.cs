19937420c057d97b23f2f88712add76fddca9248
Sergio Antonio Cortés Ocampo <sergioacortes@msn.com>
2025-08-04 16:11:25 +0200

    Configure swagger with minimal api (#4)
    Co-authored-by: sergio.cortes <sergio.cortes@wolterskluwer.com>

---
+++ src/CleanArchitecture.Api.Host/Extensions/Swagger/SwaggerAuthorizationFilter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CleanArchitecture.Api.Host.Extensions.Swagger;
+
+public sealed class SwaggerAuthorizationFilter : IOperationFilter
+{
+    private readonly IEnumerable<OpenApiSecurityRequirement> _securityRequirements;
+    private readonly OpenApiResponses _authorizedResponses;
+
+    public SwaggerAuthorizationFilter()
+    {
+        _securityRequirements = new List<OpenApiSecurityRequirement>
+        {
+            new()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "CleanArchitecture",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            },
+            new()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "apiKey",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            }
+        };
+
+        _authorizedResponses = new OpenApiResponses()
+        {
+            ["401"] = new OpenApiResponse { Description = "User not authenticated." },
+            ["403"] = new OpenApiResponse { Description = "User not authorized to perform this action." },
+        };
+    }
+
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        foreach (var securityRequirement in _securityRequirements)
+        {
+            operation.Security.Add(securityRequirement);
+        }
+
+        foreach (var response in _authorizedResponses)
+        {
+            operation.Responses.TryAdd(response.Key, response.Value);
+        }
+    }
+}
---
+++ src/CleanArchitecture.Api.Host/Routes/CompaniesEndpointsRegister.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Application.Companies.CreateCompany;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Host.Routes;
+
+public class CompaniesEndpointsRegister : IEndpointsRegister
+{
+    public void MapEndpoints(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapPost("/api/companies", async ([FromServices]IMediator mediator, [FromBody]CreateCompanyRequest request) =>
+        {
+            var result = await mediator.Send(request);
+            return Results.Ok(result);
+        });
+    }
+}
